aa608aabc72b31f5ae6268ddcdfdc19eb1ac98f9
James Stanley <james@incoherency.co.uk>
2021-01-21 21:25:02 +0000

    Update cpu.v

--- cpu.v
+++ cpu.v
@@ -14,6 +14,7 @@ module CPU(clk);
     wire [15:0] X_val;
     wire [15:0] Y_val;
     wire [15:0] A_val;
+    wire [15:0] IR_val;
     wire [15:0] PC_val;
 
     // control bits
@@ -21,16 +22,24 @@ module CPU(clk);
     // computed values (wire)
     reg [5:0] A_c;
     reg reset_bar;
-    reg EO, XI, XO, YI, YO, JZ, JNZ, JGT, JLT, JC, PO, PA;
+    reg EO, XI, XO, YI, YO, JZ, JNZ, JGT, JLT, JC, PO, PA, II, IO;
     wire JMP;
 
     // TODO: assign JMP = (JC&C) | (JZ&Z) | (JNZ&!Z) | (JGT&GT) | (JLT&!Z&!GT);
 
     ALU alu (X_val, Y_val, A_c, EO, bus, A_val);
 
-    Register XRegister (clk, bus, XI, XO, X_val);
-    Register YRegister (clk, bus, YI, YO, Y_val);
+    Register x (clk, bus, XI, XO, X_val);
+    Register y (clk, bus, YI, YO, Y_val);
 
-    PC pc(clk, bus, JMP, PO, PC_val, PA, reset_bar);
+    // TODO: "ir" needs special logic on its output to
+    // put either:
+    //   IR_val & 0xff (if IOL)
+    // or:
+    //   0xff00 | (IR_val & 0xff) (if IOH)
+    // on the bus, instead of just IR_VAL
+    Register ir (clk, bus, II, IO, IR_val);
+
+    PC pc (clk, bus, JMP, PO, PC_val, PA, reset_bar);
 
 endmodule
