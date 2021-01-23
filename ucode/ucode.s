dc0597e625358aba85bd7f74493fec888bee5283
James Stanley <james@incoherency.co.uk>
2021-01-23 21:04:09 +0000

    Add example ucode for more operations

--- ucode/ucode.s
+++ ucode/ucode.s
@@ -44,3 +44,32 @@ adc: 0b # add with carry
 
 sbc: 0c # subtract with carry (TODO: does this work or make sense?)
     EX NX F NO EO XI
+
+and: 0d # X = X&Y
+    EO XI EX EY
+
+or: 0e # X = X|Y
+    EO XI EX EY NX NY NO
+
+nand: 0f # X = ~(X&Y)
+    EO XI EX EY NO
+
+nor: 10 # X = ~(X|Y)
+    EO XI EX EY NX NY
+
+shl: 11 # X = (X<<1) = X+X (clobbers Y register)
+    XO YI         # Y = X
+    EO F          # clear carry (could potentially save a cycle with "XO YI F"? need to check)
+    EO XI EX EY F # X = X+Y
+
+# XXX: how could we implement a right-shift?
+
+xor: 12 # X = X^Y (clobbers a word in the upper page of RAM, based on the 8-bit immediate constant in the opcode)
+    # the idea here is to calculate X^Y == (X|Y) & ~(X&Y) by first storing X|Y in memory,
+    # then storing X&Y in Y, then loading the original X|Y from memory into X, then
+    # computing ~(X&Y) and storing it in X
+    IOH AI               # addr = IOH (i.e. ff..)
+    EO MI EX EY NX NY NO # M[ff..] = X|Y
+    EO YI EX EY          # Y = X&Y
+    MO XI                # X = M[ff..]
+    EO XI EX EY NO       # X = ~(X&Y)
