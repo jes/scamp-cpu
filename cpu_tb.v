e19430338e15810bdc25b94b8490ba31ceb77a59
James Stanley <james@incoherency.co.uk>
2021-01-25 16:38:14 +0000

    Add more bootrom

--- cpu_tb.v
+++ cpu_tb.v
@@ -11,7 +11,7 @@ module test;
 
     reg [15:0] cycle = 0;
 
-    parameter EXPECT_OUTPUTS = 5;
+    parameter EXPECT_OUTPUTS = 16;
     reg [15:0] outputs = 0;
 
     initial begin
@@ -33,6 +33,6 @@ module test;
             clk = 0;
         end
 
-        if (outputs !== EXPECT_OUTPUTS) $display("Bad: only got ", outputs, " outputs, expected ", EXPECT_OUTPUTS);
+        if (outputs !== EXPECT_OUTPUTS) $display("Bad: got ", outputs, " outputs, expected ", EXPECT_OUTPUTS);
     end
 endmodule
