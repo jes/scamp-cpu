e23b9528aa5996b7b4ce0bac7592e49e53bab78a
James Stanley <james@incoherency.co.uk>
2021-02-19 15:05:55 +0000

    Run self-compilation test

--- compiler/run-test.sh
+++ compiler/run-test.sh
@@ -8,3 +8,6 @@ echo "self-hosted test.sl..."
 ./minify test.sl | ./run slangc.sl > test.s
 ./srun test.s > test.out.new2
 diff test.out test.out.new && echo "ok"
+
+echo "self-compilation..."
+./selfhost-test
