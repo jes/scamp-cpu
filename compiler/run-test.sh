6bd7205c97ea68abc9453f2d9eee79a420a7bc85
James Stanley <james@incoherency.co.uk>
2021-02-19 15:02:35 +0000

    Make run-test.sh run the self-hosting compiler

--- compiler/run-test.sh
+++ compiler/run-test.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 
+echo test.sl...
 ./run test.sl > test.out.new
+diff test.out test.out.new && echo "ok"
+
+echo "self-hosted test.sl..."
+./minify test.sl | ./run slangc.sl > test.s
+./srun test.s > test.out.new2
 diff test.out test.out.new && echo "ok"
