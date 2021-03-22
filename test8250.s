20d75b5987360f8e910553e0654f348f709ce96f
James Stanley <james@incoherency.co.uk>
2021-03-22 16:22:52 +0000

    Start of test program for UART

---
+++ test8250.s
@@ -0,0 +1,25 @@
+# set divisor latches to select 115200 baud
+
+# select divisor latches:
+# write 0x80 to line control register (addr 3)
+ld x, 0x80
+out 3, x
+
+# set divisor to 1: 115200 baud
+# set high byte of divisor latch = 0
+ld x, 0
+out 1, x
+# set low byte of divisor latch = 1
+ld x, 1
+out 0, x
+
+# select data register instead of divisor latches:
+# write 0x00 to line control register (addr 3)
+ld x, 0x80
+out 3, x
+
+# write A's
+loop:
+    ld x, 65
+    out 0, x
+    jmp loop
