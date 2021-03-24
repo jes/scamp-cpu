dd047e741c636a91978a6e05364cfe29d277b9fc
James Stanley <james@incoherency.co.uk>
2021-03-24 15:59:42 +0000

    Update test8250.s

--- test8250.s
+++ test8250.s
@@ -1,25 +1,35 @@
 # set divisor latches to select 115200 baud
 
+.def REG0 0
+.def REG1 1
+.def REG3 3
+.def CLKDIVIDE 12 # 115200 / 12 = 9600 baud
+
 # select divisor latches:
-# write 0x80 to line control register (addr 3)
+# write 0x80 to line control register
 ld x, 0x80
-out 3, x
+out REG3, x
 
 # set divisor to 1: 115200 baud
 # set high byte of divisor latch = 0
 ld x, 0
-out 1, x
-# set low byte of divisor latch = 1
-ld x, 1
-out 0, x
+out REG1, x
+# set low byte of divisor latch = CLKDIVIDE
+ld x, CLKDIVIDE
+out REG0, x
 
 # select data register instead of divisor latches, and set 8-bit words, no parity, 1 stop:
 # write 0x03 to line control register (addr 3)
 ld x, 0x03
-out 3, x
+out REG3, x
 
 # write A's
 loop:
     ld x, 65
-    out 0, x
+    out REG0, x
+    ld x, 66
+    out REG0, x
+    ld x, 67
+    out REG0, x
+    ld x, 68
     jmp loop
