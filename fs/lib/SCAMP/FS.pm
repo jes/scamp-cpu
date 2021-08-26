jes/scamp-cpu

commit 6b4c4d49fe224c408c6f6848bdfa17176dfc63a0

free space bitmap: swap byte order in Perl instead of SCAMP
(Negligible performance difference, but neater this way).

fs/lib/SCAMP/FS.pm
@@ -344,6 +344,7 @@ sub findfreeblk {
         my $blknum = ($b+$self->{lastfreeblk}+1)&0xffff;
         my $blkblk = $SKIP_BLOCKS + int($blknum / ($BLKSZ * 8));
         my $byteinblk = int($blknum/8) % $BLKSZ;
+        $byteinblk ^= 1;
         my $bitinbyte = $blknum % 8;
 
         if ($blkblk != $readblk) {
@@ -366,6 +367,7 @@ sub blkisfree {
 
     my $blkblk = $SKIP_BLOCKS + int($blknum / ($BLKSZ * 8));
     my $byteinblk = int($blknum/8) % $BLKSZ;
+    $byteinblk ^= 1;
     my $bitinbyte = $blknum % 8;
 
     my @bitmapblock = $self->readblock($blkblk);
@@ -379,6 +381,7 @@ sub setblkused {
 
     my $blkblk = $SKIP_BLOCKS + int($blknum / ($BLKSZ * 8));
     my $byteinblk = int($blknum/8) % $BLKSZ;
+    $byteinblk ^= 1;
     my $bitinbyte = $blknum % 8;
 
     my @bitmapblock = $self->readblock($blkblk);

kernel/blkdev.sl
@@ -91,10 +91,6 @@ var blkfindfree = func() {
     var i = 0;
     while (FREEBLKBUF[blkgroup] & powers_of_2[i]) i++;
 
-    # upper 8 bits refer to lower 8 block numbers: swap them
-    # TODO: [perf] do this byte-swapping thing in the perl script instead of the kernel
-    i = i^8;
-
     # so now bit i in FREEBLKBUF[blkgroup] is 0, so the free block number is:
     #    (bitmapblk*4096 + blkgroup*16 + i)
     nextfreeblk = shl(bitmapblk, 12) + shl(blkgroup, 4) + i;
@@ -111,9 +107,6 @@ var blksetused = func(blk, used) {
     var bitmapblk = shr12(blk);
     var blkgroup  = byteshr4(blk & 0x0fff);
     var i         = blk & 0x0f;
-
-    # upper 8 bits refer to lower 8 block numbers: swap them
-    i = i^8;
 
     blkread(SKIP_BLOCKS + bitmapblk, FREEBLKBUF);
     if (used) *(FREEBLKBUF+blkgroup) = FREEBLKBUF[blkgroup] |  powers_of_2[i]

commit e91dc51c44748754476a3e77f8b3b1b3098843ce

mkfs: mark correct root directory block as unavailable
A bug introduced in 6b4c4d4 meant that the wrong block number was now
being marked as used, which meant /etc and / shared the same block :S.

fs/lib/SCAMP/FS.pm
@@ -344,7 +344,7 @@ sub findfreeblk {
         my $blknum = ($b+$self->{lastfreeblk}+1)&0xffff;
         my $blkblk = $SKIP_BLOCKS + int($blknum / ($BLKSZ * 8));
         my $byteinblk = int($blknum/8) % $BLKSZ;
-        $byteinblk ^= 1;
+        $byteinblk ^= 1; # swap endianness
         my $bitinbyte = $blknum % 8;
 
         if ($blkblk != $readblk) {
@@ -367,7 +367,7 @@ sub blkisfree {
 
     my $blkblk = $SKIP_BLOCKS + int($blknum / ($BLKSZ * 8));
     my $byteinblk = int($blknum/8) % $BLKSZ;
-    $byteinblk ^= 1;
+    $byteinblk ^= 1; # swap endianness
     my $bitinbyte = $blknum % 8;
 
     my @bitmapblock = $self->readblock($blkblk);
@@ -381,7 +381,7 @@ sub setblkused {
 
     my $blkblk = $SKIP_BLOCKS + int($blknum / ($BLKSZ * 8));
     my $byteinblk = int($blknum/8) % $BLKSZ;
-    $byteinblk ^= 1;
+    $byteinblk ^= 1; # swap endianness
     my $bitinbyte = $blknum % 8;
 
     my @bitmapblock = $self->readblock($blkblk);

fs/mkfs
@@ -24,9 +24,9 @@ while (<>) {
 
 push @d, 0 while @d < $FS_SIZE;
 
-# mark skipped blocks and bitmap blocks as unavailable
+# mark skipped blocks, bitmap blocks, and root directory block as unavailable
 $d[($SKIP_BLOCKS * $BLKSZ) + $_] = 0xff for (0 .. 9);
-$d[($SKIP_BLOCKS * $BLKSZ) + 10] = 0x01;
+$d[($SKIP_BLOCKS * $BLKSZ) + 11] = 0x01;
 
 $fs->{disk} = \@d;
 