b695cc8101ad5ab8dda1b06c3a83cb1c14132ae9
James Stanley <james@incoherency.co.uk>
2021-11-19 19:30:21 +0000

    aocproxy: add module for file transfer

---
+++ aoc/lib/SCAMP/Files.pm
@@ -0,0 +1,37 @@
+package SCAMP::Files;
+
+# TODO: [nice] support changing working directory
+# TODO: [bug] don't allow "../../../../../../../etc/passwd" etc. to grab arbitrary files
+
+use strict;
+use warnings;
+
+sub new {
+    my ($pkg, %args) = @_;
+
+    my $self = bless \%args, $pkg;
+
+    return $self;
+}
+
+sub attach {
+    my ($self, $serial) = @_;
+
+    $serial->handle(get => 'file' => sub {
+        my ($path) = @_;
+        open (my $fh, '<', $path)
+            or die "can't read $path: $!";
+        my $c = join('', <$fh>);
+        close $fh;
+        return $c;
+    });
+
+    $serial->handle(put => 'file' => sub {
+        my ($path, $content) = @_;
+        open (my $fh, '>', $path)
+            or die "can't write $path: $!";
+        print $fh $content;
+        close $fh;
+        return 'ok';
+    });
+}
