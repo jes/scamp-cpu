989ec497bb5ba5ddad38180332e45020371d0aed
James Stanley <james@incoherency.co.uk>
2022-07-19 00:35:45 +0100

    Generate more lib docs

---
+++ doc/lib/SLANG/Tex.pm
@@ -0,0 +1,29 @@
+package SLANG::Tex;
+
+our @ISA = qw(Exporter);
+our @EXPORT_OK = qw(texescape template slurp);
+
+sub texescape {
+    my ($str) = @_;
+    $str =~ s/([_&#^])/\\$1/g;
+    return $str;
+}
+
+sub template {
+    my ($tmpl, $fields) = @_;
+    for my $f (keys %$fields) {
+        $tmpl =~ s/$f/$fields->{$f}/g;
+    }
+    return $tmpl;
+}
+
+sub slurp {
+    my ($name) = @_;
+    open(my $fh, '<', $name)
+        or die "can't read $name: $!\n";
+    my $c = join('', <$fh>);
+    close $fh;
+    return $c;
+}
+
+1;
